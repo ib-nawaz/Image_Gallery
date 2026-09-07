0bc89c34d72aecc0c7a896de864b99d09547e8cb
Ben <ibxsusano@gmail.com>
2026-09-07 17:59:27 +0530

    Add dblclick on btn

--- script.js
+++ script.js
@@ -7,16 +7,28 @@ pics_scroll.addEventListener("wheel", (e) => {
     e.preventDefault()
 
     pics_scroll.scrollLeft -= e.deltaY;
-     
-     pics_scroll.style.scrollBehavior = "auto"
-  
+
+    pics_scroll.style.scrollBehavior = "auto"
+
 });
 
-previous.addEventListener("click",()=>{
-    pics_scroll.style.scrollBehavior ="smooth"
-       pics_scroll.scrollLeft -=300
+previous.addEventListener("click", () => {
+    pics_scroll.style.scrollBehavior = "smooth"
+    pics_scroll.scrollLeft -= 300;
 })
+
 next.addEventListener("click", () => {
     pics_scroll.style.scrollBehavior = "smooth"
-    pics_scroll.scrollLeft +=380;
-    })
+    pics_scroll.scrollLeft += 380;
+})
+
+previous.addEventListener("dblclick", () => {
+    pics_scroll.style.scrollBehavior = "smooth";
+    pics_scroll.scrollLeft -= 10550;
+
+})
+
+next.addEventListener("dblclick",()=>{
+    pics_scroll.style.scrollBehaviour = "smooth"
+    pics_scroll.scrollLeft = +10550;
+})
